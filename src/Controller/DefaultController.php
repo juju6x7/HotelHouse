9d5f4326ad0c053204c2403c1819c29846ef481c
Maarcaa <103992561+Maarcaa@users.noreply.github.com>
2022-07-28 11:18:03 +0200

    carroussel_pasfini

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Slider;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +13,14 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_home", methods={"GET"})
      */
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
         #EXERCICE : Récupérer les articles non archivés et envoyer les à la vue twig 
         // $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt'=> null]);
+       $sliders = $entityManager->getRepository(Slider::class)->findAll();
 
         return $this->render("default/home.html.twig", [
-            // 'articles'=>$articles
+            'sliders'=>$sliders
         ]);
     }
 }
