458bdbcd1ee86470dea180abb8b1488e3fddda16
alexis blas <91743625+golem427@users.noreply.github.com>
2022-07-29 10:36:09 +0200

    rename-commandeController-AdminController

--- src/Controller/AdminCommandeController.php
+++ src/Controller/AdminCommandeController.php
@@ -10,18 +10,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CommandeController extends AbstractController
+class AdminCommandeController extends AbstractController
 {
 
     /**
-    *@Route("/voir-commandes", name="show_commande", methods={"GET"})
-    */
+     *@Route("/voir-commandes", name="show_commande", methods={"GET"})
+     */
     public function showCommande(EntityManagerInterface $entityManager): Response
     {
-    $commandes = $entityManager->getRepository(Commande::class)->findAll();
-    return $this->render("/admin/admin_commande/show_commande.html.twig", [
-    'commandes' => $commandes
-    ]);
+        $commandes = $entityManager->getRepository(Commande::class)->findAll();
+
+        return $this->render("/admin/admin_commande/show_commande.html.twig", [
+            'commandes' => $commandes
+        ]);
     }
 
     /**
@@ -36,36 +37,35 @@ class CommandeController extends AbstractController
         $form = $this->createForm(CommandeFormType::class, $commande)
             ->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $commande->setCreatedAt(new DateTime());
             $commande->setUpdatedAt(new DateTime());
 
             #3 Ajout des data de commande
-             $commande->getId();
-             $commande->getFirstname();
-             $commande->getChambre();
-             $commande->getChambre();
-             $commande->getDateArrival();
-             $commande->getDateDeparture();
-             $commande->getFirstname();
-             $commande->getLastname();
-             $commande->getPhone();
-             $commande->getEmail();
-             
+            $commande->getId();
+            $commande->getFirstname();
+            $commande->getChambre();
+            $commande->getChambre();
+            $commande->getDateArrival();
+            $commande->getDateDeparture();
+            $commande->getFirstname();
+            $commande->getLastname();
+            $commande->getPhone();
+            $commande->getEmail();
 
-                $entityManager->persist($commande);
-                $entityManager->flush();
 
-                $this->addFlash('success', "Votre commande Administrateur a bien été ajoutée.");
-                return $this->redirectToRoute('show_commande');
+            $entityManager->persist($commande);
+            $entityManager->flush();
 
+            $this->addFlash('success', "Votre commande Administrateur a bien été ajoutée.");
+            return $this->redirectToRoute('show_commande');
         } # end if ($form)
 
         # 4 - Création de la vue
         return $this->render("admin\admin_commande\create_commande.html.twig", [
             'form' => $form->createView(),
-    
+
         ]);
     } # end function createCommande
 
@@ -74,48 +74,46 @@ class CommandeController extends AbstractController
      * @Route("/modifier-commande_{id}", name="update_commande", methods = {"GET|POST"})
      */
     public function updateCommande(Commande $commande, Request $request, EntityManagerInterface $entityManager): Response
-    {        
-             $form = $this->createForm(CommandeFormType::class, $commande)->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid())
     {
-      
+        $form = $this->createForm(CommandeFormType::class, $commande)->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
             $commande->setUpdatedAt(new DateTime());
 
-        $entityManager->persist($commande);
-        $entityManager->flush();
+            $entityManager->persist($commande);
+            $entityManager->flush();
 
-       
-        $this->addFlash('success', "La commande a été modifiée avec succès !");
-        return $this->redirectToRoute('show_commande');
+
+            $this->addFlash('success', "La commande a été modifiée avec succès !");
+            return $this->redirectToRoute('show_commande');
         }
         $commande = $entityManager->getRepository(Commande::class)->findById();
 
         return $this->render("admin/form/gestion_commandes.html.twig", [
             'form' => $form->createView(),
             'commande' => $commande
-        ]); 
+        ]);
     } # end funct
 
 
-     /**
+    /**
      * @Route("/archiver-commande_{id}", name="soft_delete_commande", methods={"GET"})
      */
-    public function archiverCommande(Commande $commande, EntityManagerInterface $entityManager):  Response
-    {     
+    public function archiverCommande(Commande $commande, EntityManagerInterface $entityManager): Response
+    {
         $commande->setDeletedAt(new DateTime());
         $commande->setUpdatedAt(new DateTime());
 
-            $entityManager->persist($commande);
-            $entityManager->flush();
+        $entityManager->persist($commande);
+        $entityManager->flush();
 
         $this->addFlash('success', "La commande a bien été archivée");
-        return $this->redirectToRoute("show_commande"); 
+        return $this->redirectToRoute("show_commande");
+    }
 
-}
-
- /**
+    /**
      * @Route("/restaurer-une-commande_{id}", name="restore_commande", methods={"GET"})
      */
     public function restoreCommande(Commande $commande, EntityManagerInterface $entityManager): Response
@@ -140,13 +138,4 @@ class CommandeController extends AbstractController
             'archivedCommandes' => $archivedCommandes,
         ]);
     }
-
-
 }
-
-
-
-
-
-
-
